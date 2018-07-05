8ec74cd0d3da04054f69a7b89fe84cc554784c2d
Patrick Kann <patrick@landit.dk>
2018-07-05 10:08:10 +0200

    s

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,24 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+/*$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});*/
+
+$app->post('/login', function(Request $request, Response $response, array $args) {
+    $data = array('error' => false, 'result' => []);
+    $newResponse = $response->withJson($data);
+
+    return $newResponse;
+});
+
+$app->post('/verify', function(Request $request, Response $response, array $args) {
+    $data = array('error' => false, 'result' => true);
+    $newResponse = $response->withJson($data);
+
+    return $newResponse;
 });
